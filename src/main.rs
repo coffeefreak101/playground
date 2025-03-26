f69f966f79e73bcca4e28389182f00457c9e2221
John Mears <jmears@equinix.com>
2025-03-25 21:45:38 -0600

    Add initial files

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+mod player_movement;
+
+use avian3d::math::Scalar;
+use avian3d::prelude::*;
+use bevy::prelude::*;
+use bevy::window::CursorGrabMode;
+use crate::player_movement::{CharacterControllerBundle, CharacterControllerPlugin};
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut window: Single<&mut Window>,
+) {
+    window.cursor_options.visible = false;
+    window.cursor_options.grab_mode = CursorGrabMode::Locked;
+
+    // Static physics object with a collision shape
+    commands.spawn((
+        RigidBody::Static,
+        Collider::cylinder(10.0, 0.1),
+        Mesh3d(meshes.add(Cylinder::new(10.0, 0.1))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+    ));
+
+    // Dynamic physics object with a collision shape and initial angular velocity
+    commands.spawn((
+        RigidBody::Dynamic,
+        Collider::cuboid(1.0, 1.0, 1.0),
+        AngularVelocity(Vec3::new(2.5, 3.5, 1.5)),
+        Mesh3d(meshes.add(Cuboid::from_length(1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
+        Transform::from_xyz(0.0, 4.0, 0.0),
+    ));
+
+    // Light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+
+    // Player
+    commands.spawn((
+        Mesh3d(meshes.add(Capsule3d::new(0.4, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb(0.8, 0.7, 0.6))),
+        Transform::from_xyz(0.0, 1.5, 0.0),
+        CharacterControllerBundle::new(Collider::capsule(0.4, 1.0)).with_movement(
+            30.0,
+            0.92,
+            7.0,
+            (30.0 as Scalar).to_radians(),
+        ),
+        Friction::ZERO.with_combine_rule(CoefficientCombine::Min),
+        Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
+        GravityScale(2.0),
+    ));
+}
+
+
+fn main() {
+    App::new()
+        // Enable physics
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default(), CharacterControllerPlugin))
+        .add_systems(Startup, setup)
+        .run();
+}
